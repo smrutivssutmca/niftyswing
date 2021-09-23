bdde6a7649775b16ce726a9c2f48f932ff59159f
smrutivssutmca <40881123+smrutivssutmca@users.noreply.github.com>
2021-09-24 02:23:46 +0530

    added components folder

---
+++ src/components/NiftystatsApi.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class NiftystatsApi extends Component {
+  render() {
+    return (
+      <React.Fragment>
+        <h1>Nifty Stats</h1>
+      </React.Fragment>
+    );
+  }
+}
+
+export default NiftystatsApi;
